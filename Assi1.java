ccdd5173478743005fa3941ada6d99bc8d243293
omasebabu <115346280+omasebabu@users.noreply.github.com>
2025-01-16 23:10:19 +0530

    Add files via upload

---
+++ Assi1.java
@@ -0,0 +1,11 @@
+public class ArithmeticOperator {
+	public static void main (String[]args) {
+	int a=10,b=20;
+	System.out. println("Addition is:"+ (a+b));
+	System.out.println("substraction is:"+(a-b));
+	System.out.println("Multiplication is:"+(a*b));
+	System.out.println("dividetion is :"+(a/b));
+	System.out.println("module is:"+a%b);
+
+}
+}
